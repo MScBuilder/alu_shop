3e7e4479652a479561dcbb5ebfc623616aa5b42b
Dariusz Lyssy <darek.lyssy@gmail.com>
2022-08-24 22:49:40 +0200

    - adding new graphics - adding color property to models - color property change label color accordingly

--- shop/models.py
+++ shop/models.py
@@ -5,16 +5,17 @@ from django.conf import settings
 from django.db import models
 
 CATEGORY_CHOICES = {
-    ('FW', 'fix window'),
-    ('SW', 'Single window'),
-    ('DW', 'Double window'),
-    ('LS', 'Lift-slide window') 
+    ('FW', 'Fix window'),
+    ('CW', 'Casement window'),
+    ('SW', 'Sliding window'),
 }
 
-LABEL_CHOICES = {
-    ('P', 'primary'),
-    ('S', 'secondary'),
-    ('D', 'danger')
+COLOR_CHOICES = {
+    ('9016', 'White'),
+    ('9006', 'Silver'),
+    ('9005', 'Black'),
+    ('8017', 'Brown'),
+    ('7016', 'Antracite')
 }
 
 
@@ -22,7 +23,7 @@ class Construction(models.Model):
     title = models.CharField(max_length=100)
     price = models.FloatField()
     category = models.CharField(choices=CATEGORY_CHOICES, default="FW", max_length=2)
-    label = models.CharField(choices=LABEL_CHOICES, default="P", max_length=1)
+    color = models.CharField(choices=COLOR_CHOICES, default="9016", max_length=4)
     slug = models.SlugField()
     
     def __str__(self):
